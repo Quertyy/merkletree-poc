2cac1381d80b349f75c8b4c85f966f5460fe8011
Quertyy <qwertyense@protonmail.com>
2023-01-06 14:14:38 +0100

    fix keccak256 computing

--- merkle.py
+++ merkle.py
@@ -1,4 +1,4 @@
-from Crypto.Hash import keccak
+from web3 import Web3
 import math
 
 
@@ -13,13 +13,12 @@ class MerkleTree:
 
     def __data_hash_list(self, data_list: list):
         length = len(data_list)
+
         hashes_list = [self.__compute_hash(data_list[i]) for i in range(length)]
         return hashes_list
 
     def __compute_hash(self, hash: str):
-        keccak_hash = keccak.new(digest_bits=256)
-        keccak_hash.update(hash.encode('utf-8'))
-        return keccak_hash.hexdigest()
+        return Web3.keccak(hexstr=hash).hex()
 
     def __count_enumeration(self, block_count: int):
         total_iteration = math.ceil(math.log2(block_count))
@@ -36,7 +35,15 @@ class MerkleTree:
         
 
 
-x = MerkleTree(["salut", "sa", "va", "oui", "et"])
+x = MerkleTree(
+        [
+            "0xAA6cb884573C97E0B26F771ACc0c5C94f6Ae8FFD", 
+            "0xCf185Cb08c62Ff969D258FBA18F92dabb278A8A9", 
+            "0x0b289dEa4DCb07b8932436C2BA78bA09Fbd34C44", 
+            "0xD316F2F1872648a376D8c0937db1b4b10D1Ef8b1", 
+            "54655D5468f072D5bcE1577c4a46F701C28a41A7"
+        ]
+    )
 
 print(x.data)
 print(x.hashes_list)
--- test.py
+++ test.py
@@ -1,12 +1,7 @@
-from Crypto.Hash import keccak
-import sha3
+from web3 import Web3
 
 def hash(x):
-    hash = x
-    ## keccak256 de hash
-    keccak_hash = keccak.new(digest_bits=256)
-    keccak_hash.update(hash.encode('utf-8'))
-    return keccak_hash.hexdigest()  
+    return Web3.keccak(hexstr=x).hex()
 
-print(hash("AA6cb884573C97E0B26F771ACc0c5C94f6Ae8FFD"))
+print(hash("0xAA6cb884573C97E0B26F771ACc0c5C94f6Ae8FFD"))
 
