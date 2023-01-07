323af266be7ba3f3b62e683ed7b60bb56d25b0ba
Quertyy <qwertyense@protonmail.com>
2023-01-07 14:57:25 +0100

    add sorted pairs

--- merkle.py
+++ merkle.py
@@ -4,29 +4,37 @@ import math
 
 class MerkleTree:
 
-    def __init__(self, data_list: list):
+    def __init__(self, data_list: list, sorted_pairs=False):
         self.data = data_list
         self.hashes_list = []
+        self.leaves = self.__data_hash_list(self.data)
         nb_block = len(data_list)
         (total_iteration, start_index) = self.__count_enumeration(nb_block)
-        self.iteration_count = total_iteration
         self.hashes_list.append(self.__data_hash_list(self.data))
-        
+        self.hashes_list = self.__create_merkle_tree(
+            total_iteration,
+            self.hashes_list,
+            sorted_pairs
+        )
+    
+    def __create_merkle_tree(self, total_iteration, data: list, sorted_pairs: bool):
         for i in range(total_iteration):
-            current_list = self.hashes_list[i]
+            current_list = data[i]
             list_to_hash = []
             for x, y in zip(current_list[::2], current_list[1::2]):
-                y = y[2:]
-                print(f'concat {x + y}')
+                if sorted_pairs:
+                    sorted_list = sorted([x,y])
+                    x, y = sorted_list[0], sorted_list[1][2:]
+                else :
+                    y = y[2:]
                 list_to_hash.append(x + y)
             
             hash_list = self.__data_hash_list(list_to_hash)
             if len(current_list) % 2 == 1:
                 hash_list.append(current_list[-1])
             self.hashes_list.append(hash_list)
-            
+        return self.hashes_list
 
-        
 
     def __data_hash_list(self, data_list: list):
         length = len(data_list)
@@ -41,33 +49,30 @@ class MerkleTree:
         start_index = block_count - (2**total_iteration)
         return (total_iteration, start_index)
 
-    def get_total_iteration(self):
-        return self.iteration_count
+    def get_total_hash_levels(self):
+        return self.iteration_count - 1
     
     def get_layers(self):
         return self.hashes_list
 
-    
-        
+    def get_leaves(self):
+        return self.leaves
 
-    
-        
+    def get_root(self):
+        return "".join(self.hashes_list[-1])
+
+    #def get_proof(self, data):
 
 
 x = MerkleTree(
         [
-            "0xAA6cb884573C97E0B26F771ACc0c5C94f6Ae8FFD", 
-            "0xCf185Cb08c62Ff969D258FBA18F92dabb278A8A9", 
-            "0x0b289dEa4DCb07b8932436C2BA78bA09Fbd34C44", 
-            "0xD316F2F1872648a376D8c0937db1b4b10D1Ef8b1", 
-            "0x54655D5468f072D5bcE1577c4a46F701C28a41A7",
-            "0x9a90b4ca0c5d986A6370c5C0d7985500F47C00fB",
-            "0x241055aEF1180dAFF2F8D71B5F51f77bbC564998",
-            "0xC35cAC128302CB860Ef2D3634a17025AF7a671F7",
-            "0xd1E1F5Ea4D14CbD0044F90938648eCE9d84C7Ca1",
-            "0x7eF61cAcD0C785eAcDFe17649d1c5BcBA676a858",
-            "0xf7FdEEED754b2E5fd9d826bfDf18844ee7E2D303"
-        ]
+        "0xDc3649D061897E8e826dA411a8AcB19c38152180",
+        "0x0ee60464b71eD44A18A01D4a102010835Ae809E9",
+        "0x01640032a834998cc4f8Cbd56fDD3Ab548D8383c",
+        "0x0746a63DA6ce89d409221f6D1033264FEF92F9fA",
+        "0x37eA03F5b2393F66eE491fA7eD981879931cE585"
+        ],
+        sorted_pairs=True
     )
 
 
@@ -75,4 +80,6 @@ x = MerkleTree(
 
 
 print(x.get_layers())
-print(x.get_total_iteration())
+#print(x.get_total_hash_levels())
+#print(x.get_root())
+print(x.get_leaves())
--- test.py
+++
@@ -1,7 +0,0 @@
-from web3 import Web3
-
-def hash(x):
-    return Web3.keccak(hexstr=x).hex()
-
-print(hash("0xAA6cb884573C97E0B26F771ACc0c5C94f6Ae8FFD"))
-
